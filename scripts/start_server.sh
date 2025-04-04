4b557fb169bd04214266a87d2e86d8fe8a6c2fcc
Divyam2701 <divyam.patel2701@gmail.com>
2025-04-04 17:54:22 +0530

    first commit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,8 +6,8 @@ cd /home/ubuntu/to-do-list || exit
 echo "Stopping existing PM2 process (if any)..."
 pm2 delete to-do-list || true
 
-echo "Starting Vite application using vite-node..."
-pm2 start "npx vite-node --watch src/main.jsx" --name "to-do-list"
+echo "Starting Vite server in production mode..."
+pm2 start "npx vite preview" --name "to-do-list"
 
 echo "Saving PM2 process list..."
 pm2 save
