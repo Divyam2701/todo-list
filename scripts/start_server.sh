8bb3a971fc5d9912b220838ed9ed0a7582e9a47a
Divyam2701 <divyam.patel2701@gmail.com>
2025-04-04 17:43:11 +0530

    first commit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-cd /home/ubuntu/to-do-list
-pm2 stop all
+
+echo "Navigating to project directory..."
+cd /home/ubuntu/to-do-list || exit
+
+echo "Stopping existing PM2 processes (if any)..."
+pm2 delete to-do-list || true  # Prevents error if process doesn't exist
+
+echo "Starting the Vite application..."
 pm2 start npm --name "to-do-list" -- run dev
+
+echo "Saving PM2 process list..."
 pm2 save
-pm2 startup
+
+echo "Setting PM2 to start on reboot..."
+pm2 startup systemd -u ubuntu --hp /home/ubuntu | tail -1 | bash
