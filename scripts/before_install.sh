6a686693e53c5282d935698de43020f522216e6d
Divyam2701 <divyam.patel2701@gmail.com>
2025-04-07 14:17:01 +0530

    docker command

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,8 +3,4 @@
 echo "Starting Docker service..."
 sudo systemctl start docker
 
-echo "Stopping and removing old container (if exists)..."
-sudo docker stop todo-container || true
-sudo docker rm todo-container || true
-
 echo "System ready for new container deployment."
